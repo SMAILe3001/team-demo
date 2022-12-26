260125ce77c55612fee83c9270ad9019f9704ffb
SMAILe3001 <114938645+SMAILe3001@users.noreply.github.com>
2022-12-26 13:50:32 +0000

    Deploying to gh-pages from @ SMAILe3001/team-demo@7b8930749591b0e45f971deaa93dedb69aa1dae1 🚀

--- index.71633a9d.js
+++
@@ -1,2 +0,0 @@
-$(".slick-carousel").slick({dots:!0,infinite:!0,speed:500,slidesToShow:1,slidesToScroll:1});
-//# sourceMappingURL=index.71633a9d.js.map
--- index.f33cd132.js
+++
@@ -1,2 +0,0 @@
-$(".slick-carousel").slick({dots:!0,infinite:!0,speed:500,slidesToShow:1,slidesToScroll:1});
-//# sourceMappingURL=index.f33cd132.js.map
